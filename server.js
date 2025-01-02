69505c9d410a5d4116bb0869522e7eef53b64085
EmD4blju <mikolaj.warda@gmail.com>
2025-01-02 18:49:56 +0100

    Added functional HTTP POST & GET endpoints

--- server.js
+++ server.js
@@ -1,14 +1,102 @@
 const express = require('express')
 const server = express()
 const PORT = 3000
+require('dotenv').config()
+server.use(express.json())
+
+let gps_storage = {
+    '21ac8ced-310c-4766-ba28-1ecb6c85b566': {
+        'lat': 50.342,
+        'lon': 21.567,
+        'timestamp': Date.now()
+    },
+    '99ad32a2-9fdf-4895-9d41-febef5d0de2e': {
+        'lat': 100.52,
+        'lon': 260.123,
+        'timestamp': Date.now()
+    },
+    '0ca28dae-f426-4d03-bc5d-06e73906f57e': {
+        'lat': 50.342,
+        'lon': 21.567,
+        'timestamp': Date.now()
+    }
+}
+
 
 server.post('/gps', (req, res) => {
-// TODO: need to add posting coordinates functionality
+
+    const { gps_id, lat, lon } = req.body
+
+    gps_storage[gps_id] = {
+        'lat': lat,
+        'lon': lon,
+        'timestamp': Date.now()
+    }
+
+    res.json({
+        message: 'Data received & stored successfully!',
+        data: gps_id
+    });
 })
 
 
 server.get('/view', (req, res) => {
-// TODO: need to add view of the gps tracker in the GMaps IFrame
+    html_response = `
+        <table>
+            <thead>
+                <tr>
+                    <td>GPS_ID</td>
+                    <td>LAT</td>
+                    <td>LON</td>
+                    <td>TIMESTAMP</td>
+                </tr>
+            </thead>
+            <tbody>
+    `
+    for (let gps_id in gps_storage){
+        lat = gps_storage[gps_id]['lat']
+        lon = gps_storage[gps_id]['lon']
+        timestamp = gps_storage[gps_id]['timestamp']
+        html_response += `
+                <tr>
+                    <td><a href='/view/${gps_id}'>${gps_id}</a></td>
+                    <td>${lat}</td>
+                    <td>${lon}</td>
+                    <td>${timestamp}</td>
+                </tr>
+        `
+    }
+
+    html_response += `
+            </tbody>
+        </table>
+    `
+
+    res.send(html_response)
+})
+
+server.get('/view/:gps_id', (req, res) => {
+
+    gps_id = req.params.gps_id
+
+    received_gps = {
+        'gps_id': gps_id,
+        'lat': gps_storage[gps_id]['lat'],
+        'lon': gps_storage[gps_id]['lon']
+    }
+
+    html_response = `
+        <iframe
+            width="600"
+            height="450"
+            style="border:0;"
+            loading="lazy"
+            allowfullscreen
+            src="https://www.google.com/maps/embed/v1/place?key=${process.env.API_KEY}&q=${received_gps['lat']}, ${received_gps['lon']}">
+        </iframe>
+    `
+
+    res.send(html_response)
 })
 
 
