7dd8612743a77e726de65c636c1b86186b818c59
EmD4blju <mikolaj.warda@gmail.com>
2025-01-02 13:50:31 +0100

    Setup endpoints for development

--- server.js
+++ server.js
@@ -2,6 +2,17 @@ const express = require('express')
 const server = express()
 const PORT = 3000
 
+server.post('/gps', (req, res) => {
+// TODO: need to add posting coordinates functionality
+})
+
+
+server.get('/view', (req, res) => {
+// TODO: need to add view of the gps tracker in the GMaps IFrame
+})
+
+
+
 server.listen(PORT,
     (error) => {
         if(!error){
