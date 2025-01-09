fd131720a80629a337b3ad9b5d2063dae77e53b5
EmD4blju <mikolaj.warda@gmail.com>
2025-01-10 00:49:39 +0100

    Added formatted timestamp

--- server.js
+++ server.js
@@ -1,43 +1,27 @@
 const express = require('express')
+const date = require('date-and-time');
 const server = express()
-require('dotenv').config()
 server.use(express.json())
+require('dotenv').config()
 const PORT = process.env.PORT
 
-let gps_storage = {
-    '21ac8ced-310c-4766-ba28-1ecb6c85b566': {
-        'lat': 50.342,
-        'lon': 21.567,
-        'timestamp': Date.now()
-    },
-    '99ad32a2-9fdf-4895-9d41-febef5d0de2e': {
-        'lat': 100.52,
-        'lon': 260.123,
-        'timestamp': Date.now()
-    },
-    '0ca28dae-f426-4d03-bc5d-06e73906f57e': {
-        'lat': 50.342,
-        'lon': 21.567,
-        'timestamp': Date.now()
-    }
-}
+let gps_storage = {}
 
 server.post('/gps', (req, res) => {
     const { gps_id, lat, lon } = req.body
     gps_storage[gps_id] = {
         'lat': lat,
         'lon': lon,
-        'timestamp': Date.now()
+        'timestamp': date.format(new Date(), 'DD/MM/YYYY HH:mm:ss')
     }
     res.json({
-        message: 'Data received & stored successfully!',
+        message: 'RECEIVED_DATA_STATUS: REGISTERED',
         data: gps_id
     });
 })
 
 
 server.get('/view', (req, res) => {
-    console.log("Received GET /view")
     html_response = `
         <head>
             <meta http-equiv="refresh" content="5" >
@@ -123,11 +107,11 @@ server.listen(PORT,
     (error) => {
         if(!error){
             console.log(
-                `Server is running on port ${PORT}`
+                `SERVER_STATUS: RUNNING\nSERVER_PORT: ${PORT}`
             )
         }else{
             console.log(
-                `Error occured: ${error}`
+                `ERROR: ${error}`
             )
         }
     }
